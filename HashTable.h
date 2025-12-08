518882cc928f7dba4e8f2a378cfdfbd9d3fc63ef
LREMGAR <lremgar@etsinf.upv.es>
2025-12-08 11:09:36 +0100

    Añadida implementación de la clase HashTable

--- HashTable.h
+++ HashTable.h
@@ -43,6 +43,41 @@ public:
     int capacity() {
         return max;
     }
+	
+
+    //funcion insertar clave-valor
+    void insert(std::string key, V value){
+	int pos = h(key);
+	TableEntry<V> entry(key,value);
+	
+	if(!table[pos].search(entry)){ //si no existe 
+		table[pos].append(entry);
+		n++;
+	} else{ //si existe, actualizamos valor
+		int idx = table[pos].indexOf(entry);
+		table[pos].set(idx, entry);
+	}	
+    }
+
+    //funcion buscar clave
+    bool search(std::string key){
+    	int pos = h(key);
+	TableEntry<V> entry(key, V());
+	return table[pos].search(entry);
+    }
+
+    //funcion eliminar clave
+    void remove(std::string key){
+    	int pos = h(key);
+	TableEntry<V> entry(key, V());
+	if(table[pos].search(entry)){ //si pos no esta vacia
+		int idx = table[pos].indexOf(entry);
+		table[pos].remove(idx);
+		n--;
+	} else{
+		throw std::runtime_error("Clave no encontrada");
+	}
+    }
 
     // Sobrecarga operador <<
     friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th) {
@@ -57,7 +92,7 @@ public:
     V operator[](std::string key) {
         int pos = h(key);
         // Buscar en la lista correspondiente
-        for (int i = 0; i < table[pos].size(); i++) {
+        for (int i = 0; i < table[pos].size(); i++) { 
             TableEntry<V> entry = table[pos].get(i);
             if (entry.key == key) {
                 return entry.value;
