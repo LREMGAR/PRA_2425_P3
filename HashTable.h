b7b508b9509c493c4c0a1b9072b34e5e2cd1fa61
LREMGAR <lremgar@etsinf.upv.es>
2025-12-13 16:13:53 +0100

    Añadida implementación de la clase HashTable

--- HashTable.h
+++ HashTable.h
@@ -11,97 +11,112 @@
 
 template <typename V>
 class HashTable : public Dict<V> {
+    private:
+        int n;   // número de elementos almacenados
+        int max; // número total de cubetas
+        ListLinked<TableEntry<V>>* table; // array de listas enlazadas
 
-private:
-    int n;   // número de elementos almacenados
-    int max; // número total de cubetas
-    ListLinked<TableEntry<V>>* table; // array de listas enlazadas
-
-    // Función hash: suma de ASCII % max
-    int h(std::string key) {
-        int sum = 0;
-        for (size_t i = 0; i < key.size(); i++) {
-            sum += int(key.at(i));
+        // Función hash: suma de ASCII % max
+        int h(std::string key) {
+            int sum = 0;
+            for (size_t i = 0; i < key.size(); i++) {
+                sum += int(key.at(i));
+            }
+            return sum % max;
         }
-        return sum % max;
-    }
 
-public:
-    // Constructor
-    HashTable(int size) {
-        max = size;
-        n = 0;
-        table = new ListLinked<TableEntry<V>>[max];
-    }
-
-    // Destructor
-    ~HashTable() {
-        delete[] table;
-    }
-
-    // Capacidad total
-    int capacity() {
-        return max;
-    }
-	
-
-    //funcion insertar clave-valor
-    void insert(std::string key, V value){
-	int pos = h(key);
-	TableEntry<V> entry(key,value);
-	
-	if(!table[pos].search(entry)){ //si no existe 
-		table[pos].append(entry);
-		n++;
-	} else{ //si existe, actualizamos valor
-		int idx = table[pos].indexOf(entry);
-		table[pos].set(idx, entry);
-	}	
-    }
-
-    //funcion buscar clave
-    bool search(std::string key){
-    	int pos = h(key);
-	TableEntry<V> entry(key, V());
-	return table[pos].search(entry);
-    }
-
-    //funcion eliminar clave
-    void remove(std::string key){
-    	int pos = h(key);
-	TableEntry<V> entry(key, V());
-	if(table[pos].search(entry)){ //si pos no esta vacia
-		int idx = table[pos].indexOf(entry);
-		table[pos].remove(idx);
-		n--;
-	} else{
-		throw std::runtime_error("Clave no encontrada");
-	}
-    }
-
-    // Sobrecarga operador <<
-    friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th) {
-        for (int i = 0; i < th.max; i++) {
-            out << "Cubeta " << i << ": ";
-            out << th.table[i] << std::endl; // suponiendo que ListLinked<T> sobrecarga <<
-        }
-        return out;
-    }
-
-    // Sobrecarga operador []
-    V operator[](std::string key) {
-        int pos = h(key);
-        // Buscar en la lista correspondiente
-        for (int i = 0; i < table[pos].size(); i++) { 
-            TableEntry<V> entry = table[pos].get(i);
-            if (entry.key == key) {
-                return entry.value;
+    public:
+        // Constructor
+        HashTable(int size): n(0), max(size), table(nullptr) {
+            if(size <= 0) throw std::runtime_error("Tamaño de la tabla invalido.");
+            
+            table = new ListLinked<TableEntry<V>>*[max];
+            for(int i = 0; i < max; i++){
+                table[i] = new ListLinked<TableEntry<V>>();
             }
         }
-        throw std::runtime_error("Clave no encontrada en la tabla hash");
+
+        // Destructor
+        ~HashTable() {
+            delete[] table;
+        }
+
+        // Capacidad total
+        int capacity() {
+            return max;
+        }
+
+         // Sobrecarga operador <<, imprimir contenido de la tabla
+        friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th) {
+            out << "HashTable [entries: " << th.n << ", capacity: " << th.max << "]" << endl;
+            for (int i = 0; i < th.max; i++) {
+                out << "Cubeta " << i << ": " << endl;
+                //out << th.table[i] << std::endl; // suponiendo que ListLinked<T> sobrecarga <<
+                out << "List => " << th.table[i] << endl << endl;
+            }
+            out << "\n"<< endl;
+            return out;
+        }
+
+        // Sobrecarga operador [], devuelve el valor de la clave
+        V operator[](std::string key) {
+            return search(key);
+        }
 	
-     }	
-    };
+        //metodos heredados 
+        //funcion insertar clave-valor
+        void insert(std::string key, V value) override{
+	        int pos = h(key);
+	        ListLinked<TableEntry<V>>& posicion = table[pos];
+	
+	        for (int i = 0; i < table[pos].size(); i++) {
+                if (table[pos].get(i).key == key) {
+                    throw runtime_error("Key: " + key + ",ya existe");
+                }
+            }
+
+            //en caso de aun no exista esa clave, crear un tableEntry completo y lo añade al final de la lista
+            table[pos].append(TableEntry<V>(key, value));
+            n++;	
+        }
+
+        int entries() override{
+            return n;
+        }
+
+        V search(std::string key) override {
+            int pos = h(key);
+            ListLinked<TableEntry<V>>& posicion = table[pos];
+            
+            for (int i = 0; i < posicion.size(); i++) {
+                TableEntry<V> entry = posicion.get(i);
+                if(entry.key == key){
+                    return entry.value;
+                }
+            }
+
+            throw std::runtime_error("Key '" + key + "', no se ha encontrado.");
+        }
+
+        //funcion eliminar clave
+        V remove(std::string key) override{
+    	    int pos = h(key);
+            ListLinked<TableEntry<V>>& posicion = table[pos];
+
+	        for (int i = 0; i < posicion.size(); i++) {
+                TableEntry<V> entry = posicion.get(i);
+                if (entry.key == key) {
+                    V value = entry.value;
+                    posicion.remove(i);
+                    n--;
+                    return value;
+                }  
+            }
+            throw std::runtime_error("Key '" + key + "', no se ha encontrado.");
+        }
+	
+            
+};
 
 #endif
 
